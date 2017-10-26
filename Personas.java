4047c2eb64792bd395050c8bb8b6d65ddc2d82a7
Nere <lilinere97@gmail.com>
2017-10-26 12:09:50 -0500

    puse un mensajito

--- Personas.java
+++ Personas.java
@@ -3,7 +3,7 @@ class Personas{
   String apellidos=null;
   String correo=null;
   String tel=null;
-
+//mensajito
   Personas(String linea){
     String Temporal[]=linea.split(",");
     this.nombres=Temporal[1];
