c08253b48d4003aeeae1afd88d65350a442ef975
Nereyda97 <32875861+Nereyda97@users.noreply.github.com>
2017-10-26 14:09:22 -0500

    nuevos cambios
    le quite lo de mostrar las listas de alumnos y materias   
    le agrege el while y el menu

--- Interfaz.java
+++ Interfaz.java
@@ -1,7 +1,41 @@
+import java.util.Scanner;
 class Interfaz{
-  //PROGRAMA PRINCIPAL
+
+  //PROGRAMA PRINCIPAL PARA MATERIAS Y ALUMNOS
   public static void main(String[] args){
-    alumno A=new alumno();
-    A.importar();
+    boolean opcion=true;
+          Scanner T=new Scanner(System.in);
+          int respuesta=0;
+    while(opcion){
+      System.out.println("Facultad");
+      System.out.println("1. Crear");
+      System.out.println("2. Modificar");
+      System.out.println("3. Consultar");
+      System.out.println("4. Salir");
+      System.out.println("-------------------------");
+      System.out.print(">>> ");
+
+      respuesta=T.nextInt();
+    }
+    switch(respuesta){
+      case 1:
+      System.out.println("1. Crear");
+      break;
+
+      case 2:
+      System.out.println("2. Modificar");
+      break;
+
+      case 3:
+      System.out.println("3. Consultar");
+      break;
+
+      case 4:
+      opcion=false;
+      break;
+
+      default:
+      break;
+    }
   }
 }
