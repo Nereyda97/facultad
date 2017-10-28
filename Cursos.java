024b20b6460ce77d4d6199e044d89d5c7d3ac5e4
irisbruno621 <33131962+irisbruno621@users.noreply.github.com>
2017-10-28 00:31:57 -0500

    listo
    me faltan los salones

--- Cursos.java
+++ Cursos.java
@@ -1,8 +1,33 @@
-class Cursos{
-  int Total = 0;
- public calculadora(Alumno A){
-   //Mensajito
- return Total;
-   //HOLA
- }
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursos;
+
+
+public class Cursos {
+
+  
+    public static void main(String[] args) {
+
+    String[] Cursos = {"Curso1", "Curso2", "Curso3", "Curso4", "Curso5", "Curso6", "Curso7",
+        " Curso8", "Curso9", "Curso10"};  
+    
+    int [] Clave = {10040, 10041, 10042, 10043, 10044, 10045, 10046, 10047, 10048, 10049};
+    
+    String[] Profesores ={"M.C. LUZ ELENA ARÉVALO LEÓN", "M.C. ROSA ELENA TORRES RUIZ ",
+"QFB. RUTH CALDERÓN VÁZQUEZ", "D.C. ESTHER GARCÍA GARIBAY ", "QFB. RICARDO VEGA TAVERA", 
+"M.F.B. KARLA LILIANA GARCÍA CHÁVEZ", "QFB. OSCAR ANTONIO MUÑOS LOPES", "I.BQ. RODRIGO MERLOS ROJAS", 
+"D.C. SANDRA GUADALUPE SANCHEZ CEJA", "QFB. SANDRA MARÍA SUÁREZ MORENO", "M.C. ALMA ROSA GARCÍA RÍOS",
+"D.C. JUDIT ARACELI AVIÑA VERDUZCO", "QFB. RICARDO VEGA TAVERA","M.F.B. KARLA LILIANA GARCÍA CHÁVEZ",
+"QFB. OSCAR ANTONIO LOPES MUÑOS","I.BQ. RODRIGO MERLOS ROJAS", "QFB. RICARDO VEGA TAVERA", 
+"M.F.B. KARLA LILIANA GARCÍA CHÁVEZ", "QFB. OSCAR ANTONIO LOPES MUÑOS","I.BQ. RODRIGO MERLOS ROJAS",
+"M.C. ROSA MA. GARCIA MARTINEZ", "QFB. MARTHA JOSEFINA DE LOS DOLORES TORRES","D.Q. MARIO ARMANDO GÓMEZ HURTADO",
+"M.C. LUIS RAÙL CHÁVEZ GARIBAY", "M.F.B. KARLA LILIANA GARCÍA CHÁVEZ",
+"QFB. OSCAR ANTONIO MUÑOS LOPES","I.BQ. RODRIGO MERLOS ROJAS"};
+ 
+    
+    int [] AlumnosInscritos = { 20, 30,25, 45, 15, 23, 32, 45, 21, 36}; 
+    }
 }
