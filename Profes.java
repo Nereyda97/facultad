e4cbd8a7363e044d9e1066bf85011dabcd38ef04
lenglez <33131866+lenglez@users.noreply.github.com>
2017-10-29 00:02:19 -0500

    Update Profes.java

--- Profes.java
+++ Profes.java
@@ -1,21 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package profes;
 
-/**
- *
- * @author Marlen
- */
+import java.util.Scanner;
 public class Profes {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+     
+    public static void main(String[] args) {
+       String[] Profesor = {"M.C. LUZ ELENA AREVALO","M.C. ROSA ELENA TORRES RUIZ","QFB. RUTH CALDERON VELAZQUEZ",
+"D.C. ESTHER GARCIA GARIBAY","QFB. RICARDO VEGA TAVERA", "M.F.B. KARLA LILIANA GARCIA CHAVEZ","QFB. RAMON ANTONIO MUÃ‘OS LOPES",
+"I.BQ. RODRIGO MERLOS ROJAS","D.C. SANDRA GUADALUPE SANCHEZ CEJA","QFB. SANDRA MARIA SUAREZ MORENO", "M.C. ALMA ROSA GARCIA RAMOS",
+"D.C. JUDIT ARACELI AVILA VERDUZCO","QFB. RICARDO VEGA TAVERA","M.F.B. KARLA LILIANA GARCIA CHAVEZ","QFB. OSCAR ANTONIO LOPES MUÃ‘OS",
+"I.BQ. RODRIGO MERLOS ROJAS","QFB. RICARDO VEGA TAVERA","M.F.B. KARLA LILIANA GARCIA CHAVEZ","I.BQ. RODRIGO MERLOS ROJAS","M.C. ROSA MA. GARCIA MARTINEZ", 
+"QFB. MARTHA JOSEFINA DE LOS DOLORES TORRES","D.Q. MARIO ARMANDO GÃƒâ€œMEZ HURTADO","M.C. LUIS RAFAEL CHAVEZ GARIBAY","M.F.B. KARLA LILIANA GARCIA CHAVEZ"
+};
+       int[] A = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24 };
+       int[] B = {0,1,3,2,3,0,1,2,3,0,0,3,2,1,3,2,2,1,2,3,4,2,1,0};
+       
+       
+            Scanner T = new Scanner(System.in);
+            System.out.println("Ingrese un nombre: ");
+            String x = T.nextLine();
+            for( int i=0; i<=24; i++){
+               if ( x.equals(Profesor[i])){ 
+                   System.out.println("EL nombre es " + Profesor[i] +" \n " + A[i] + " " + B[i]);
+               }
+                }   
+        
+        
+        
+        
+    }
 }
